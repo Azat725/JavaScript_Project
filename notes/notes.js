896cf191cfeae5516afb1e3fcd01610bf6429b36
Azat725 <152897735+Azat725@users.noreply.github.com>
2024-01-26 22:29:08 +0300

    qwerty

---
+++ notes/notes.js
@@ -0,0 +1,9 @@
+const inputElement = document.getElementById('title')
+const createBtn = document.getElementById('create')
+const listElement = document.getElementById('list')
+
+createBtn.onclick = function() {
+    listElement.innerHTML = `
+    
+    `
+}
