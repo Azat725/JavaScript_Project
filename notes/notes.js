cc6abb7e27660cc20378734d8878b7e9d5f8b38f
Azat725 <152897735+Azat725@users.noreply.github.com>
2024-01-27 13:02:54 +0300

    2:14:00

--- notes/notes.js
+++ notes/notes.js
@@ -2,8 +2,34 @@ const inputElement = document.getElementById('title')
 const createBtn = document.getElementById('create')
 const listElement = document.getElementById('list')
 
-createBtn.onclick = function() {
-    listElement.innerHTML = `
-    
-    `
+const notes = []
+
+function render() {
+  for (let i = 0; i < notes.length; i++) {
+    listElement.insertAdjacentHTML('beforeend', getNoteTemplate(notes)) 
+  }
+}
+
+render()
+
+createBtn.onclick = function () {
+    listElement.insertAdjacentHTML('beforeend', 
+    getNoteTemplate(inputElement.value)
+    )
+    inputElement.value = ' '
+}
+
+function getNoteTemplate(title) {
+  return `
+    <li
+    class="list-group-item d-flex justify-content-between 
+    align-items-center"
+  >
+    <span>${title}</span>
+    <span>
+      <span class="btn btn-small btn-success">&check;</span>
+      <span class="btn btn-small btn-danger">&times;</span>
+    </span>
+  </li>
+  `
 }
